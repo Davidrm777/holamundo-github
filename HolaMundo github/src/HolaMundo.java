4ba770f7adc5aa0399052e31258aa96fc237a836
Davidrm777 <98969679+Davidrm777@users.noreply.github.com>
2022-02-10 16:54:43 +0100

    Update HolaMundo.java

--- HolaMundo github/src/HolaMundo.java
+++ HolaMundo github/src/HolaMundo.java
@@ -8,6 +8,6 @@ class HolaMundo
     public static void main(String args[])
     {
         System.out.println("Hola Mundo");
-        //añade aqui otro System.out.println con tu nombre completo
+        System.out.println("David Ruiz Montero");
     }
 }
